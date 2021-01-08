a5650f4f894b2fdf561d6eb779d09ac3bbedf209
baron <linsiyao.lsy@lazada.com>
2021-01-08 14:12:34 +0800

    chapter6 baton

---
+++ chapter6/05baton/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var (
+	wg sync.WaitGroup
+)
+
+func main() {
+
+	baton := make(chan int)
+
+	wg.Add(1)
+
+	go Runner(baton)
+
+	baton <- 1
+
+	wg.Wait()
+}
+
+func Runner(baton chan int) {
+	defer wg.Done()
+
+	runner := <- baton
+
+	fmt.Printf("Runner %d running\n", runner)
+
+	if runner == 4 {
+		fmt.Printf("Runner %d finished, Race Over\n", runner)
+		close(baton)
+		return
+	}
+
+	// 启动线程必须在写入通道之前，因为该通道是没有缓冲区的
+	wg.Add(1)
+	go Runner(baton)
+
+	runner++
+	baton<-runner
+}
---
+++ chapter6/06worker/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"sync"
+	"fmt"
+	"time"
+)
+
+var (
+	wg sync.WaitGroup
+	taskNum = 10
+	workerNum = 4
+)
+
+func main() {
+	tasks := make(chan int, taskNum)
+
+	wg.Add(workerNum)
+
+	for i := 1; i <= workerNum; i++ {
+		go worker(tasks, i)
+	}
+
+	for i := 1; i <= taskNum; i++ {
+		tasks <- i
+	}
+
+	// 关闭了通道但是还是可以从通道中获取数据
+	close(tasks)
+
+	wg.Wait()
+}
+
+func worker(tasks chan int, id int) {
+	defer wg.Done()
+	for {
+		task, ok := <- tasks
+		if !ok {
+			fmt.Printf("Worker %d done all the tasks\n", id)
+			return
+		}
+
+		time.Sleep(time.Millisecond * 200)
+
+		fmt.Printf("Worker %d done task %d\n", id, task)
+	}
+}
